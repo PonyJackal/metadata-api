e3e0a1dd1f8e8fe242267ba88c7596f458218f47
ponyjackal <pony1125@outlook.com>
2022-05-10 20:17:08 -0400

    feat: create tokens and attributes table

---
+++ src/database/migrations/20220511001052_create_table_tokens.ts
@@ -0,0 +1,18 @@
+import { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+    return knex.schema.createTable('tokens', (table: Knex.TableBuilder) => {
+        table.uuid('id').primary().notNullable().unique();
+        table.string('description').notNullable();
+        table.string('external_url').notNullable();
+        table.string('image').notNullable();
+        table.string('name').notNullable();
+        table.timestamps(true, true);
+    });
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+}
+
---
+++ src/database/migrations/20220511001100_create_table_attributes.ts
@@ -0,0 +1,24 @@
+import { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+    return knex.schema.createTable('attributes', (table: Knex.TableBuilder) => {
+        table.uuid('id').primary().notNullable().unique();
+        table.string('trait_type').notNullable();
+        table.string('value').notNullable();
+        table.string('display_type').notNullable();
+
+        table
+            .uuid('token_id')
+            .notNullable()
+            .references('id')
+            .inTable('tokens');
+
+        table.timestamps(true, true);
+    });
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+}
+
